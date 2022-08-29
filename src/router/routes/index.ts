71121542a1eb04948c200a036879b2cd4c3f2bc7
zolay <leon@sjlei.com>
2022-08-29 10:57:16 +0800

    删除无用代码，减小打包体积

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -2,7 +2,6 @@ import type { AppRouteRecordRaw, AppRouteModule } from '/@/router/types';
 
 import { PAGE_NOT_FOUND_ROUTE, REDIRECT_ROUTE } from '/@/router/routes/basic';
 
-import { mainOutRoutes } from './mainOut';
 import { PageEnum } from '/@/enums/pageEnum';
 import { t } from '/@/hooks/web/useI18n';
 
@@ -38,12 +37,21 @@ export const LoginRoute: AppRouteRecordRaw = {
   },
 };
 
+export const GuobaRoute: AppRouteRecordRaw = {
+  name: 'MasterLogin',
+  path: '/ml/:code',
+  component: () => import('/@/views/guoba/login/MasterLogin.vue'),
+  meta: {
+    title: t('routes.basic.login'),
+  },
+}
+
 // Basic routing without permission
 // 未经许可的基本路由
 export const basicRoutes = [
   LoginRoute,
   RootRoute,
-  ...mainOutRoutes,
   REDIRECT_ROUTE,
   PAGE_NOT_FOUND_ROUTE,
+  GuobaRoute,
 ];
