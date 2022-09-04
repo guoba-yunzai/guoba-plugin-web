483b1d5775b39b6688515aaddfacea3280131ff8
sjlei <leon@sjlei.com>
2022-09-04 16:56:35 +0800

    完善randomImage

--- src/views/guoba/home/components/SaleRadar.vue
+++ src/views/guoba/home/components/SaleRadar.vue
@@ -1,96 +1,65 @@
 <template>
-  <Card title="随机美图" :loading="loading">
-    <!--    <div ref="chartRef" :style="{ width, height }"></div>-->
-    <PageCoding />
+  <Card class="random-iamge-card" title="随机美图" style="min-height: 300px">
+    <a-spin :spinning="loading" style="min-height: 150px">
+      <div class="img" :style="divStyle" />
+    </a-spin>
   </Card>
 </template>
 <script lang="ts" setup>
-  import { Ref, ref, watch } from 'vue';
+  import { computed, ref } from 'vue';
   import { Card } from 'ant-design-vue';
-  import { useECharts } from '/@/hooks/web/useECharts';
-  import PageCoding from '/@/components/Guoba/src/PageCoding.vue';
+  import { defHttp } from '/@/utils/http/axios';
 
-  const props = defineProps({
-    loading: Boolean,
-    width: {
-      type: String as PropType<string>,
-      default: '100%',
-    },
-    height: {
-      type: String as PropType<string>,
-      default: '400px',
-    },
+  const imgSrc = ref('');
+  const loading = ref(false);
+  const imgHeight = ref(500);
+
+  const divStyle = computed(() => {
+    let style: Recordable = {};
+    style.backgroundImage = `url(${imgSrc.value})`;
+    style.height = `${imgHeight.value}px`;
+    return style;
   });
 
-  const chartRef = ref<HTMLDivElement | null>(null);
-  const { setOptions } = useECharts(chartRef as Ref<HTMLDivElement>);
-  watch(
-    () => props.loading,
-    () => {
-      if (props.loading) {
-        return;
+  async function loadImage() {
+    try {
+      loading.value = true;
+      let res = await defHttp.get(
+        { url: '/home/random-image', responseType: 'blob' },
+        { isTransformResponse: false, isReturnNativeResponse: true },
+      );
+      let img = new Image();
+      imgSrc.value = URL.createObjectURL(res.data);
+      img.src = imgSrc.value;
+      await new Promise((resolve) => (img.onload = resolve));
+      let height = img.height;
+      if (height > 500) {
+        height = 500;
       }
-      setOptions({
-        legend: {
-          bottom: 0,
-          data: ['Visits', 'Sales'],
-        },
-        tooltip: {},
-        radar: {
-          radius: '60%',
-          splitNumber: 8,
-          indicator: [
-            {
-              name: '2017',
-            },
-            {
-              name: '2017',
-            },
-            {
-              name: '2018',
-            },
-            {
-              name: '2019',
-            },
-            {
-              name: '2020',
-            },
-            {
-              name: '2021',
-            },
-          ],
-        },
-        series: [
-          {
-            type: 'radar',
-            symbolSize: 0,
-            areaStyle: {
-              shadowBlur: 0,
-              shadowColor: 'rgba(0,0,0,.2)',
-              shadowOffsetX: 0,
-              shadowOffsetY: 10,
-              opacity: 1,
-            },
-            data: [
-              {
-                value: [90, 50, 86, 40, 50, 20],
-                name: 'Visits',
-                itemStyle: {
-                  color: '#b6a2de',
-                },
-              },
-              {
-                value: [70, 75, 70, 76, 20, 85],
-                name: 'Sales',
-                itemStyle: {
-                  color: '#67e0e3',
-                },
-              },
-            ],
-          },
-        ],
-      });
-    },
-    { immediate: true },
-  );
+      imgHeight.value = height;
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  loadImage();
+
+  // @ts-ignored
+  // noinspection JSUnusedGlobalSymbols
+  const _components = { Card, divStyle };
 </script>
+<style lang="less">
+  .random-iamge-card {
+    .ant-card-body {
+      padding: 8px;
+
+      div.img {
+        width: 100%;
+        height: 500px;
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+      }
+    }
+  }
+</style>
