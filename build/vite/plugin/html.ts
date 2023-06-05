279d133dd627f2228b2d4e3e8b1bd47ee56f087c
zolay <moe@zozo.me>
2023-06-05 11:47:05 +0800

    允许动态加载preload

--- build/vite/plugin/html.ts
+++ build/vite/plugin/html.ts
@@ -2,19 +2,37 @@
  * Plugin to minimize and use ejs template syntax in index.html.
  * https://github.com/anncwb/vite-plugin-html
  */
-import type { PluginOption } from 'vite';
+import type { PluginOption, HtmlTagDescriptor } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import pkg from '../../../package.json';
 import { GLOB_CONFIG_FILE_NAME } from '../../constant';
 
 export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
-  const { VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH } = env;
+  const {VITE_GLOB_APP_TITLE, VITE_PUBLIC_PATH} = env;
 
   const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
 
   const getAppConfigSrc = () => {
     return `${path || '/'}${GLOB_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
   };
+
+  const tags: HtmlTagDescriptor[] = []
+  if (isBuild) {
+    tags.push({
+      tag: 'script',
+      attrs: {
+        src: getAppConfigSrc(),
+      },
+    })
+  } else {
+    // Guoba模拟preload.js
+    tags.push({
+      tag: 'script',
+      attrs: {
+        src: "/preload/_app.guoba.preload.js",
+      },
+    })
+  }
 
   const htmlPlugin: PluginOption[] = createHtmlPlugin({
     minify: isBuild,
@@ -24,16 +42,7 @@ export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
         title: VITE_GLOB_APP_TITLE,
       },
       // Embed the generated app.config.js file
-      tags: isBuild
-        ? [
-            {
-              tag: 'script',
-              attrs: {
-                src: getAppConfigSrc(),
-              },
-            },
-          ]
-        : [],
+      tags: tags
     },
   });
   return htmlPlugin;
