3cbe4f608be587ab2f731eaa5e33eebd4c54ba84
zolay <moe@zozo.me>
2025-01-03 18:11:17 +0800

    fix: 修复打包后登陆界面背景丢失

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -83,7 +83,7 @@
       background-color: @dark-bg;
 
       &::before {
-        background-image: url(/@/assets/svg/login-bg-dark.svg);
+        background-image: url(/assets/svg/login-bg-dark.svg);
       }
 
       .ant-input,
@@ -129,7 +129,7 @@
       width: 100%;
       height: 100%;
       margin-left: -48%;
-      background-image: url(/@/assets/svg/login-bg.svg);
+      background-image: url(/assets/svg/login-bg.svg);
       background-position: 100%;
       background-repeat: no-repeat;
       background-size: auto 100%;
