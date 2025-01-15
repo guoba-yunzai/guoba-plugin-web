f4f99a1b8a9593bf9ad5284bbd7ddec5836f4705
zolay <moe@zozo.me>
2025-01-16 01:18:11 +0800

    fix: 重名路由

--- src/router/routes/basic.ts
+++ src/router/routes/basic.ts
@@ -5,7 +5,7 @@ import { REDIRECT_NAME, LAYOUT, EXCEPTION_COMPONENT, PAGE_NOT_FOUND_NAME } from 
 // 404 on a page
 export const PAGE_NOT_FOUND_ROUTE: AppRouteRecordRaw = {
   path: '/:path(.*)*',
-  name: PAGE_NOT_FOUND_NAME,
+  name: PAGE_NOT_FOUND_NAME + '_LAYOUT',
   component: LAYOUT,
   meta: {
     title: 'ErrorPage',
@@ -31,7 +31,7 @@ export const REDIRECT_ROUTE: AppRouteRecordRaw = {
   component: LAYOUT,
   name: 'RedirectTo',
   meta: {
-    title: REDIRECT_NAME,
+    title: REDIRECT_NAME + '_LAYOUT',
     hideBreadcrumb: true,
     hideMenu: true,
   },
